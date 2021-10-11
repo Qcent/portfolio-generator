93aba506681a28af1e905d496ba9e5422518abca
Dave Quinn <dquinn8@cogeco.ca>
2021-10-11 10:48:56 -0400

    optimized code with arrow function one liner

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const printProfileData = (profileDataArr) => {
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
+    profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
