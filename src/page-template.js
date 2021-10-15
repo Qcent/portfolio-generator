ece274a25460156eb947c67a3d08df81086269fd
Dave Quinn <dquinn8@cogeco.ca>
2021-10-14 20:37:24 -0400

    ajit helped me turn off my auto formater for java html and css it was causing the problems with the template literals

--- src/page-template.js
+++ src/page-template.js
@@ -1,10 +1,10 @@
 // create the about section
 const generateAbout = aboutText => {
-    if (!aboutText) {
-        return '';
-    }
+  if (!aboutText) {
+    return '';
+  }
 
-    return `
+  return `
     <section class="my-3" id="about">
       <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
       <p>${aboutText}</p>
@@ -14,49 +14,40 @@ const generateAbout = aboutText => {
 
 // create the projects section
 const generateProjects = projectsArr => {
-    return `
+  return `
     <section class="my-3" id="portfolio">
       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
       <div class="flex-row justify-space-between">
       ${projectsArr
-        .filter(({ feature }) => feature)
-        .map(({ name, description, languages, link }) => {
-          return ` <
-        div class = "col-12 mb-2 bg-dark text-light p-3" >
-        <
-        h3 class = "portfolio-item-title text-light" > $ { name } < /h3> <
-        h5 class = "portfolio-languages" >
+      .filter(({ feature }) => feature)
+      .map(({ name, description, languages, link }) => {
+        return ` 
+        <div class = "col-12 mb-2 bg-dark text-light p-3" >
+        <h3 class = "portfolio-item-title text-light" > ${name} </h3> 
+        <h5 class = "portfolio-languages" >
         Built With:
-        $ { languages.map(language => language).join(',') } <
-        /h5> <
-        p > $ { description } < /p> <
-        a href = "${link}"
-    class = "btn" > < i class = "fab fa-github mr-2" > < /i>View Project on GitHub</a >
-        <
-        /div>
+        ${languages.map(language => language).join(',')} </h5>
+        <p> ${description} </p> 
+        <a href = "${link}" class = "btn"> <i class = "fab fa-github mr-2"> </i>View Project on GitHub</a>
+        </div>
     `;
-        })
-        .join('')}
+      })
+      .join('')}
 
       ${projectsArr
-        .filter(({ feature }) => !feature)
-        .map(({ name, description, languages, link }) => {
-          return ` <
-    div class = "col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column" >
-        <
-        h3 class = "portfolio-item-title text-light" > $ { name } < /h3> <
-        h5 class = "portfolio-languages" >
+      .filter(({ feature }) => !feature)
+      .map(({ name, description, languages, link }) => {
+        return ` 
+        <div class = "col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column" >
+        <h3 class = "portfolio-item-title text-light" > ${name} </h3> 
+        <h5 class = "portfolio-languages" >
         Built With:
-        $ { languages.join(', ') } <
-        /h5> <
-        p > $ { description } < /p> <
-        a href = "${link}"
-    class = "btn mt-auto" > < i class = "fab fa-github mr-2" > < /i>View Project on GitHub</a >
-        <
-        /div>
+        ${languages.join(', ')} </h5> <p> ${description} </p> 
+        <a href = "${link}" class = "btn mt-auto"> <i class = "fab fa-github mr-2"> </i>View Project on GitHub</a>
+        </div>
     `;
-        })
-        .join('')}
+      })
+      .join('')}
       </div>
     </section>
   `;
@@ -64,11 +55,11 @@ const generateProjects = projectsArr => {
 
 // export function to generate entire page
 module.exports = templateData => {
-    console.log(templateData);
-    // destructure projects and about data from templateData based on their property key names
-    // this will create three variables based on data in templateData
-    const { projects, about, ...header } = templateData;
-    return `
+  console.log(templateData);
+  // destructure projects and about data from templateData based on their property key names
+  // this will create three variables based on data in templateData
+  const { projects, about, ...header } = templateData;
+  return `
     <!DOCTYPE html>
     <html lang="en">
   
@@ -87,9 +78,8 @@ module.exports = templateData => {
         <div class="container flex-row justify-space-between align-center py-3">
           <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
           <nav class="flex-row">
-            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-              header.github
-            }">GitHub</a>
+            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github
+    }">GitHub</a>
           </nav>
         </div>
       </header>
